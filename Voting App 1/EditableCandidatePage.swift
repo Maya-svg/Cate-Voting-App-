1dd1a923432c3e014d1fc480a1503978dbaf2e95
Maya-svg <mayaf388@gmail.com>
2020-05-18 12:55:32 -0400

    add selection buttons to edit page

--- Voting App 1/EditableCandidatePage.swift
+++ Voting App 1/EditableCandidatePage.swift
@@ -12,29 +12,64 @@ struct EditableCandidatePage: View {
     @State var txt = ""
     @ObservedObject var candidateProfiles: AppManager
     @Environment(\.presentationMode) var tfPresentationMode
+    
+    @State private var didTap:Bool = false
+    @State private var didTap2:Bool = false
+    @State private var didTap3:Bool = false
+    
     var body: some View {
         ZStack{
             VStack{
-                
-                Text("change the")
-                Text("change the promises")
                 MultiLineTF(txt: self.$txt)
                     .border(Color.black, width: 3)
                     .padding()
+                
+                VStack(alignment: .leading){
                 Button(action: {
+                    self.didTap = true
                     self.candidateProfiles.details[0] = self.txt
                     //how can i make it dependant on who it is?
                     print( "self.candidateProfiles.details[0] is \(self.candidateProfiles.details[0])")
                     //how can i  show this data on the regular page?
                 }) {
-                    Text("change the bio")
+                    Image(systemName: "circle")
+                    Text("Change The Bio Text")
+                }.foregroundColor(didTap ? Color.green : Color.black)
+                
+                Button(action: {
+                    self.didTap2 = true
+                    self.candidateProfiles.details[1] = self.txt
+                    //how can i make it dependant on who it is?
+                    print( "self.candidateProfiles.details[1] is \(self.candidateProfiles.details[1])")
+                    //how can i  show this data on the regular page?
+                }) {
+                    Image(systemName: "circle")
+                    Text("Change The Goals Text")
+                }.foregroundColor(didTap2 ? Color.green : Color.black)
+                
+                Button(action: {
+                    self.didTap3 = true
+                    self.candidateProfiles.details[2] = self.txt
+                    //how can i make it dependant on who it is?
+                    print( "self.candidateProfiles.details[2] is \(self.candidateProfiles.details[2])")
+                    //how can i  show this data on the regular page?
+                }) {
+                    Image(systemName: "circle")
+                    Text("Change The Promises Text")
+                }.foregroundColor(didTap3 ? Color.green : Color.black)
                 }
+                .padding(.bottom,10)
                 Button(action: {
                     print("Added text is stored")
                     self.tfPresentationMode.wrappedValue.dismiss()
-                    print( "self.candidateProfiles.details[0] is \(self.candidateProfiles.details[0])")
+                    self.didTap = false
+                    self.didTap2 = false
+                    self.didTap3 = false
                 }) {
                     Text("Publish Text")
+                    .font(.system (size: 25))
+                        .fontWeight(.heavy)
+
                 }//.onDisappear(){self.candidateProfiles.details[0]}
                 //how can i make this stored ariable appear on the candidate page? 
             }
@@ -56,9 +91,7 @@ struct MultiLineTF : UIViewRepresentable {
         tview.isScrollEnabled = true
         tview.text = "Type your information"
         tview.textColor = .gray
-        
         tview.delegate = context.coordinator
-        print("This is where I am")
         return tview
     }
     
